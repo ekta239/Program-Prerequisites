bdb27332326e28bf208b120ebac699617b253167
Ekta verma <ektav8231@gmail.com>
2025-01-08 12:10:01 +0530

    ninth solution

---
+++ Q9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Q9 {
+public static void main(String[]args) {
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter first number: ");
+int num1=sc.nextInt();
+System.out.print("Enter second number: ");
+int num2=sc.nextInt();
+System.out.print("Enter third number: ");
+int num3=sc.nextInt();
+float a=(num1+num2+num3)/3;
+System.out.println("Average of numbers: " +a);
+}
+}
