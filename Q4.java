9be3447b30a5addc4a9834803d00103b4dee72da
Ekta verma <ektav8231@gmail.com>
2025-01-08 11:06:17 +0530

    fourth solution

---
+++ Q4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Q4 {
+public static void main(String[]args) {
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter radius of circle: ");
+double r = sc.nextDouble();
+double A= 3.14*r*r;
+System.out.println("Area of Circle: " + A);
+}
+}
